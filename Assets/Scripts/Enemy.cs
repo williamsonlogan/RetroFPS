5a78f7c1c80837fdfd8bb030ba2d1508431df7c6
williamsonlogan <williamson.logan@gmail.com>
2017-03-02 20:29:02 -0800

    Work on enemy AI, NEEDS WORK

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -6,7 +6,15 @@ public class Enemy : MonoBehaviour {
 	public float hp = 2;
 	public float damage = 5;
 	public float speed = 2;
+	public float range = 10;
 
+	bool playerInRange = false;
+	bool playerInSight = false;
+	bool playerSeen = false;
+
+	public LineRenderer lineRenderer = new LineRenderer();
+
+	public GameObject player;
 	// Use this for initialization
 	void Start () {
 		
@@ -14,7 +22,31 @@ public class Enemy : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (playerInRange) {
+			transform.LookAt (player.transform);
+				RaycastHit hit;
+
+			if(Physics.Raycast(transform.position, player.transform.position, out hit, range))
+				{
+				//Debug outline of raycast
+				lineRenderer.SetPosition (0, new Vector3(transform.position.x, transform.position.y, transform.position.z));
+				lineRenderer.SetPosition (1, hit.point);
+
+					if (hit.collider.tag == "Player") {
+						playerInSight = true;
+						if (!playerSeen)
+							playerSeen = true;
+						Debug.Log ("Player in sight!");
+					} 
+				}
+		}
+
+		if (playerSeen && !playerInSight) {
+			transform.position = Vector3.MoveTowards (transform.position, player.transform.position, speed * Time.deltaTime);
+			playerInSight = false;
+		}
+
+		transform.position = new Vector3 (transform.position.x, 0.8f, transform.position.z);
 	}
 
 	void TakeDamage(float dmg){
@@ -22,6 +54,14 @@ public class Enemy : MonoBehaviour {
 
 		if (hp <= 0) {
 			GameObject.Destroy (gameObject);
+		}
+	}
+
+	void OnTriggerEnter (Collider other)
+	{
+		if (other.GetComponent<Collider>().tag == "Player") {
+			Debug.Log ("Player in range!");
+			playerInRange = true;
 		}
 	}
 }
