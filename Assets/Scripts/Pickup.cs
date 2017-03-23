0e1fea7006c888aea3035770ed4145568f2fb21e
williamsonlogan <williamson.logan@gmail.com>
2017-03-22 17:57:40 -0700

    origin and pickup start

---
+++ Assets/Scripts/Pickup.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour {
+
+	public int health; //Added Health
+	public int bat; //Added Battery
+	public int armor; //Added Armor
+	public Firearms[] gun; //Gun pickup
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- Assets/Scripts/WeaponManager/Firearms.cs
+++ Assets/Scripts/WeaponManager/Firearms.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 
+[System.Serializable]
 public class Firearms
 {
 	// Player Weapons
