e7d91decf1ceda1b3f24e7c9b8e9c6881dca8198
williamsonlogan <williamson.logan@gmail.com>
2017-03-24 20:47:02 -0700

    Laser Fix

--- Assets/Scripts/LevelExit.cs
+++ Assets/Scripts/LevelExit.cs
@@ -9,8 +9,7 @@ public class LevelExit : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		Cursor.lockState = CursorLockMode.None;
-		Cursor.visible = true;
+		
 	}
 	
 	// Update is called once per frame
