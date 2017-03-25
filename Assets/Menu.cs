ff7c9d10d5b2f3ff3b31fd0f0f0af4bd2d87e20b
williamsonlogan <williamson.logan@gmail.com>
2017-03-24 17:19:59 -0700

    Demo Prep

--- Assets/Menu.cs
+++ Assets/Menu.cs
@@ -1,12 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour {
 
+	public int width = 1280, height = 720;
+	public bool full = false;
+
+	public Button settingsButton;
+	public Button playButton;
+	public Button backButton;
+
+	public GameObject main;
+	public GameObject settings;
+
 	// Use this for initialization
 	void Start () {
-		
+		Screen.SetResolution (width, height, full);
+		settings.gameObject.SetActive (false);
 	}
 	
 	// Update is called once per frame
@@ -14,7 +27,23 @@ public class Menu : MonoBehaviour {
 		
 	}
 
+	void Play () {
+		SceneManager.LoadScene ("01");
+	}
+
 	void Quit () {
 		Application.Quit ();
+	}
+
+	void OpenSettings()
+	{
+		settings.gameObject.SetActive (true);
+		main.gameObject.SetActive (false);
+	}
+
+	void CloseSettings()
+	{
+		settings.gameObject.SetActive (false);
+		main.gameObject.SetActive (true);
 	}
 }
